28523fe87edc7af29d0a4c38387c1459454cb7da
Mark <mverleg.noreply@gmail.com>
2022-02-27 22:07:13 +0100

    Error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,33 @@
 use ::std::path::Path;
-use std::path::PathBuf;
+use ::std::path::PathBuf;
+use ::std::process::exit;
 
 use ::ignore::WalkBuilder;
 
-pub fn make_ignore_walker(pth: impl AsRef<Path>) -> Vec<PathBuf> {
-    WalkBuilder::new(pth.as_ref())
+pub fn make_ignore_walker(root: impl AsRef<Path>) -> Vec<PathBuf> {
+    let paths = WalkBuilder::new(root.as_ref())
         .standard_filters(false)
         .follow_links(true)
-        //.add_ignore(".git")
         .add_custom_ignore_filename(".gitignore")
         .add_custom_ignore_filename(".dockerignore")
         .add_custom_ignore_filename(".backupignore")
         .parents(true)
         .build()
         .into_iter()
-        .map(|pth| pth.unwrap().path().to_path_buf())
-        .collect::<Vec<_>>()
+        .map(|pth| pth.map(|p| p.path().to_path_buf()))
+        .filter(|pth| pth.as_ref().map(|p| p.is_file()).unwrap_or(false))
+        .collect::<Result<Vec<_>, _>>();
+    match paths {
+        Ok(paths) => {
+            if paths.is_empty() {
+                eprintln!("did not find any file that is not ignored in '{}'", root.as_ref().to_string_lossy());
+                exit(1)
+            }
+            paths
+        },
+        Err(err) => {
+            eprintln!("failed to find files: {}", err);
+            exit(1)
+        }
+    }
 }
