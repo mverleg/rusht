c0ffee4ed118150ba2eed2112bd7974426ae1cde
Mark <mverleg.noreply@gmail.com>
2022-03-02 22:30:27 +0100

    Get rid of iterators to try and fix Pattern lifetimes

--- src/merge_ignores/main.rs
+++ src/merge_ignores/main.rs
@@ -24,18 +24,25 @@ async fn main() {
 }
 
 fn find_ignore_patterns(pth: &str) -> Vec<Pattern> {
-    make_ignore_walker(pth).into_iter()
-        .filter(|pth|
-            pth.file_name()
-                .map(|name| name.to_string_lossy())
-                .filter(|name| name.starts_with(".") && name.ends_with("ignore"))
-                .is_some()
-        )
-        .map(|pth| (pth.as_path(), read_to_string(&pth).unwrap_or_else(|err| stop!("failed to read ignore file; err: {}", err))))
-        .flat_map(|(pth, content)| content.lines().map(|line| (pth, line)))
-        .filter(|(_, line)| PATTERN_RE.is_match(*line))
-        .map(|(pth, line)| parse_pattern(line, pth))
-        .collect::<Vec<_>>()
+    let mut patterns = vec![];
+    for pth in make_ignore_walker(pth) {
+        let is_ignore = pth.file_name()
+            .map(|name| name.to_string_lossy())
+            .filter(|name| name.starts_with(".") && name.ends_with("ignore"))
+            .is_some();
+        if !is_ignore {
+            continue;
+        }
+        let content = read_to_string(&pth)
+            .unwrap_or_else(|err| stop!("failed to read ignore file; err: {}", err));
+        for line in content.lines() {
+            if !PATTERN_RE.is_match(line) {
+                continue;
+            }
+            patterns.push(parse_pattern(line, pth.as_path()));
+        }
+    }
+    patterns
 }
 
 fn parse_pattern<'a>(line: &str, ignore_pth: &'a Path) -> Pattern<'a> {
