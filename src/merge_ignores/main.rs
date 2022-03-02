c0ffeedcc7604f0c86bd9d3ca9c9f15bbcbb3797
Mark <mverleg.noreply@gmail.com>
2022-03-02 22:37:15 +0100

    Separately collect paths to try and solve lifetimes, but did not work because its lifetime of ignore file path, not parent dir

--- src/merge_ignores/main.rs
+++ src/merge_ignores/main.rs
@@ -1,5 +1,6 @@
 use ::std::fs::read_to_string;
 use ::std::path::Path;
+use std::path::PathBuf;
 
 use ::bump_alloc::BumpAlloc;
 use ::gitignore::Pattern;
@@ -19,20 +20,23 @@ static A: BumpAlloc = BumpAlloc::with_size(1024 * 1024 * 4);
 
 #[tokio::main]
 async fn main() {
-    let _ignore_patterns = find_ignore_patterns("./");
+    let ignores = collect_ignore_paths("./");
+    let _ignore_patterns = find_ignore_patterns(&ignores);
     //TODO @mark:
 }
 
-fn find_ignore_patterns(pth: &str) -> Vec<Pattern> {
-    let mut patterns = vec![];
-    for pth in make_ignore_walker(pth) {
-        let is_ignore = pth.file_name()
+pub fn collect_ignore_paths(pth: &str) -> Vec<PathBuf> {
+    return make_ignore_walker(pth).into_iter()
+        .filter(|pth| pth.file_name()
             .map(|name| name.to_string_lossy())
             .filter(|name| name.starts_with(".") && name.ends_with("ignore"))
-            .is_some();
-        if !is_ignore {
-            continue;
-        }
+            .is_some())
+        .collect::<Vec<_>>()
+}
+
+fn find_ignore_patterns(paths: &[PathBuf]) -> Vec<Pattern> {
+    let mut patterns = vec![];
+    for pth in paths {
         let content = read_to_string(&pth)
             .unwrap_or_else(|err| stop!("failed to read ignore file; err: {}", err));
         for line in content.lines() {
