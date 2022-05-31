a61989a5c0b6460b2a1e7e576e6b31ce8fc39038
Mark <mverleg.noreply@gmail.com>
2022-05-31 19:17:03 +0200

    Create test for adding and running tasks

--- rusht_cmd/src/tests.rs
+++ rusht_cmd/src/tests.rs
@@ -1,11 +1,17 @@
-use std::io::stdin;
+use ::std::io::stdin;
+use ::std::sync::Once;
+
 use ::rand::Rng;
 use ::rand::thread_rng;
 
-use crate::{add_cmd, AddArgs, AddArgsExtra, drop_cmd, DropArgs, list_cmds, ListArgs};
+use crate::{add_cmd, AddArgs, AddArgsExtra, do_cmd, DoArgs, drop_cmd, DropArgs, list_cmds, ListArgs};
+
+static INIT: Once = Once::new();
 
 fn init_test() -> String {
-    env_logger::init();
+    INIT.call_once(|| {
+        env_logger::init();
+    });
     let mut rng = thread_rng();
     let namespace = format!("unit_test_{}", rng.gen::<u32>());
     drop_cmd(DropArgs {
@@ -48,6 +54,55 @@ fn batch_add_drop() {
         all: true,
         count: 0,
         end: false,
+        quiet: false,
+    });
+    let out = list_cmds(ListArgs {
+        namespace,
+        file_path: false,
+        count: None,
+        exit_code: true
+    });
+    assert!(out.is_err());
+}
+
+#[test]
+fn onebyone_add_run() {
+    let namespace = init_test();
+    add_cmd(AddArgs {
+        namespace: namespace.to_string(),
+        quiet: false,
+        end: false,
+        skip_validation: false,
+        lines: false,
+        lines_with: None,
+        cmd: AddArgsExtra::Cmd(vec!["echo".to_owned(), "bye".to_owned(), "world".to_owned()])
+    }, || vec![]);
+    add_cmd(AddArgs {
+        namespace: namespace.to_string(),
+        quiet: false,
+        end: false,
+        skip_validation: false,
+        lines: false,
+        lines_with: None,
+        cmd: AddArgsExtra::Cmd(vec!["echo".to_owned(), "hello".to_owned(), "world".to_owned()])
+    }, || vec![]);
+    let out = list_cmds(ListArgs {
+        namespace: namespace.to_owned(),
+        file_path: false,
+        count: None,
+        exit_code: false
+    }).unwrap();
+    assert_eq!(out, vec![
+        "echo hello world  # 1".to_owned(),
+        "echo bye world  # 2".to_owned(),
+    ]);
+    do_cmd(DoArgs {
+        namespace: namespace.to_owned(),
+        count: 1,
+        autorun: true,
+        parallel: false,
+        always_pop: false,
+        keep: false,
         quiet: false,
     });
     let out = list_cmds(ListArgs {
