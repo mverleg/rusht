2827696f68859783fcbdd4ebd9b0b04f9097517f
Mark <mverleg.noreply@gmail.com>
2022-06-01 10:55:08 +0200

    Cache can show help successfully

--- src/cached/cached.rs
+++ src/cached/cached.rs
@@ -1,13 +1,8 @@
-use ::std::fs;
-use ::std::path::PathBuf;
 use ::std::time::Duration;
+use std::process::ExitCode;
 
-use ::itertools::Itertools;
-use ::regex::Regex;
+use ::parse_duration::parse as parse_dur;
 use ::structopt::StructOpt;
-use ::ustr::Ustr;
-
-use crate::find::unique_prefix;
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -15,10 +10,10 @@ use crate::find::unique_prefix;
     about = "Cache the output of a command for a given duration, running it only if there is no cache or it has expired."
 )]
 pub struct CachedArgs {
-    #[structopt(parse(try_from_str = parse_duration), short = "d", long = "duration", default_value = "15 min", help = "Duration the cache should be valid for, e.g. \"30 min\" or \"1 day -1 hour\".")]
+    #[structopt(parse(try_from_str = parse_dur), short = "d", long = "duration", default_value = "15 min", help = "Duration the cache should be valid for, e.g. \"30 min\" or \"1 day -1 hour\".")]
     pub duration: Duration,
-    #[structopt(short = "k", long = "key", default_value = "${pwd}_${cmd}.cache", help = "The key to use for the cache. Can use ${pwd} and ${cmd} placeholders.")]
-    pub key: Duration,
+    #[structopt(short = "k", long = "key", default_value = "${pwd}_${cmd}.cache", help = "The key to use for the cache. Can use ${pwd} and ${cmd} placeholders. If it contains a / it will be considered a full path.")]
+    pub key: String,
     #[structopt(subcommand)]
     pub cmd: CachedArgsExtra,
 }
@@ -30,7 +25,16 @@ pub enum CachedArgsExtra {
     Cmd(Vec<String>),
 }
 
-fn parse_duration(txt: &str) -> Result<Duraton, String> {
-    unimplemented!();
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CacheString {
+    RanSuccessfully,
+    FromCache,
+    Failed(u8),
 }
 
+fn cached(args: CachedArgs) -> Result<CacheString, String> {
+    //TODO @mark: The key to use for the cache. Can use ${pwd} and ${cmd} placeholders. If it contains a / it will be considered a full path.
+
+
+    unimplemented!()
+}
--- src/cached/main_cached.rs
+++ src/cached/main_cached.rs
@@ -2,22 +2,11 @@ use ::std::process::exit;
 
 use ::structopt::StructOpt;
 
-use ::rusht::find::DirWithArgs;
-use ::rusht::find::find_dir_with;
+use ::rusht::cached::CachedArgs;
 
 fn main() {
     env_logger::init();
     let args = CachedArgs::from_args();
     dbg!(&args);  //TODO @mark:
-    match find_dir_with(args) {
-        Ok(lines) => {
-            for line in lines {
-                println!("{}", line);
-            }
-        }
-        Err(err) => {
-            eprintln!("{}", err);
-            exit(1)
-        }
-    }
+    //TODO @mark:
 }
