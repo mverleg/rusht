438a611d8a64c9dbd67471a997c94672b2e30893
Mark <mverleg.noreply@gmail.com>
2022-06-27 23:35:32 +0200

    Optimize imports, handle errors

--- src/filter/main_grab.rs
+++ src/filter/main_grab.rs
@@ -1,9 +1,8 @@
 use ::std::io::{BufRead, BufReader, stdin};
+use ::std::process::exit;
 
 use ::clap::StructOpt;
-use ::ustr::Ustr;
 
-use ::rusht::common::{EmptyLineHandling, stdin_lines};
 use ::rusht::filter::{unique, unique_prefix, UniqueArgs};
 use ::rusht::filter::{grab, GrabArgs};
 
@@ -12,5 +11,11 @@ fn main() {
     let args = GrabArgs::from_args();
     let mut lines = BufReader::new(stdin().lock()).lines();
     let line_supplier = || lines.next();
-    grab(args, line_supplier, |line| println!("{}", line));
+    match grab(args, line_supplier, |line| println!("{}", line)) {
+        Ok(()) => {}
+        Err(err) => {
+            eprintln!("{}", err);
+            exit(1);
+        },
+    }
 }
