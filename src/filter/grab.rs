79c44331d0b056e6835154fcc04e75071e8f75b0
Mark <mark@fake>
2022-06-27 18:42:15 +0200

    First test for grab

--- src/filter/grab.rs
+++ src/filter/grab.rs
@@ -6,12 +6,13 @@ use ::regex::Regex;
 
 #[derive(StructOpt, Debug)]
 #[structopt(
-    name = "grab",
-    about = "Filter lines by regular expression, keeping only the matching capture group."
+name = "grab",
+about = "Filter lines by regular expression, keeping only the matching capture group."
 )]
 pub struct GrabArgs {
     //TODO @mverleg: keep unmatching lines
     //TODO @mverleg: able to match multiple times per line
+    //TODO @mverleg: case-insensitive
     #[structopt(help = "Regular expression to match. Returns the capture group if any, or the whole match otherwise.")]
     pub pattern: Regex,
 }
@@ -22,7 +23,11 @@ fn test_cli_args() {
     GrabArgs::into_app().debug_assert()
 }
 
-pub fn grab(args: GrabArgs, mut consumer: impl FnMut(String), mut line_supplier: fn() -> Option<io::Result<String>>) -> Result<(), String> {
+pub fn grab(
+    args: GrabArgs,
+    mut line_supplier: impl FnMut() -> Option<io::Result<String>>,
+    mut consumer: impl FnMut(String),
+) -> Result<(), String> {
     while let Some(line_res) = line_supplier() {
         let line = match line_res {
             Ok(line) => line,
@@ -36,4 +41,29 @@ pub fn grab(args: GrabArgs, mut consumer: impl FnMut(String), mut line_supplier:
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn run_grab(input: Vec<String>) -> Vec<String> {
+        let args = GrabArgs {
+            pattern: Regex::new("(a+)b").unwrap(),
+        };
+        let mut res = vec![];
+        let mut lines = input.into_iter().map(|v| Ok(v));
+        grab(
+            args,
+            || lines.next(),
+            |line| res.push(line)).unwrap();
+        res
+    }
+
+    #[test]
+    fn test_empty() {
+        let res = run_grab(vec![]);
+        let expected: Vec<String> = vec![];
+        assert_eq!(res, expected);
+    }
 }
--- src/filter/main_grab.rs
+++ src/filter/main_grab.rs
@@ -10,7 +10,7 @@ use ::rusht::filter::{grab, GrabArgs};
 fn main() {
     env_logger::init();
     let args = GrabArgs::from_args();
-    let mut reader = BufReader::new(stdin().lock());
-    let line_supplier = || reader.lines().next();
-    grab(args, |line| println!("{}", line), line_supplier);
+    let mut lines = BufReader::new(stdin().lock()).lines();
+    let line_supplier = || lines.next();
+    grab(args, line_supplier, |line| println!("{}", line));
 }
