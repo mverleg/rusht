658d3586974e2bf7aaeef9677a5733f152d35388
Mark <mverleg.noreply@gmail.com>
2022-07-12 23:06:16 +0200

    When piping operations, make sure to intertwine future executions with `join`

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,15 @@ pub mod escape;
 mod tests {
     use ::std::cmp::max;
 
-    use ::async_std::channel::bounded;
     use ::async_std::channel::{Receiver, Sender};
+    use ::async_std::channel::bounded;
     use ::async_trait::async_trait;
+    use ::futures::future::join;
+    use ::futures::FutureExt;
     use ::regex::Regex;
 
     use crate::common::{LineReader, LineWriter, VecReader, VecWriter};
-    use crate::filter::{grab, unique, GrabArgs, Keep, Order, UniqueArgs};
+    use crate::filter::{grab, GrabArgs, Keep, Order, unique, UniqueArgs};
 
     #[async_std::test]
     async fn chain_inout() {
@@ -34,7 +36,6 @@ mod tests {
             first_only: true,
             keep_unmatched: true,
         };
-        grab(grab_args, &mut inp1, &mut out1).await.unwrap();
 
         let mut out2 = VecWriter::new();
         let unique_args = UniqueArgs {
@@ -43,7 +44,10 @@ mod tests {
             by: None,
             prefix: true,
         };
-        unique(unique_args, &mut inp2, &mut out2).await;
+        join(
+            grab(grab_args, &mut inp1, &mut out1).map(|v| v.unwrap()),
+            unique(unique_args, &mut inp2, &mut out2),
+        ).await;
 
         out2.assert_eq(vec!["world", "Mars", "Venus", "bye"]);
     }
@@ -57,6 +61,7 @@ mod tests {
     impl LineWriter for ChainWriter {
         async fn write_line(&mut self, line: impl AsRef<str> + Send) {
             let line = line.as_ref().to_owned();
+            eprintln!("chain write: {}", &line);  //TODO @mark: TEMPORARY! REMOVE THIS!
             self.sender.send(line).await.unwrap()
         }
     }
@@ -70,14 +75,16 @@ mod tests {
     #[async_trait]
     impl LineReader for ChainReader {
         async fn read_line(&mut self) -> Option<&str> {
+            eprintln!("chain read start");  //TODO @mark: TEMPORARY! REMOVE THIS!
             self.current = self.receiver.recv().await.unwrap();
+            eprintln!("chain read done: {}", &self.current);  //TODO @mark: TEMPORARY! REMOVE THIS!
             Some(&self.current)
         }
     }
 
     //TODO @mark: move to common read/write
     fn chained(buffer_size: usize) -> (ChainWriter, ChainReader) {
-        let buffer_size = max(1, buffer_size);
+        let buffer_size = max(2, buffer_size);
         let (sender, receiver) = bounded(buffer_size);
         (
             ChainWriter { sender },
