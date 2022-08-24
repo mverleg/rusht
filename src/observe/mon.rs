b81636dddc9469556598f0877eff8661bd9cf6b2
Mark Verleg <mark.verleg@imc.com>
2022-08-24 20:05:00 +0200

    Trying to make rodio play notification

--- src/observe/mon.rs
+++ src/observe/mon.rs
@@ -1,14 +1,45 @@
+use ::std::io::Cursor;
+
+use ::log::debug;
+use ::rodio::{Decoder, OutputStream, Source};
+
 use crate::common::{LineReader, LineWriter};
-use crate::observe::mon_args::MonArgs;
 use crate::ExitStatus;
+use crate::observe::mon_args::MonArgs;
 
 pub async fn mon(
     args: MonArgs,
     _reader: &mut impl LineReader,
     _writer: &mut impl LineWriter,
 ) -> ExitStatus {
-    let task = args.cmd.into_task();
+    let task = args.cmd.clone().into_task();
     let status = task.execute(false);
+    if let Err(err) = sound_notification(&args, status.success()) {
+        eprintln!("failed to play sound: {}", err);
+        return ExitStatus::err()
+    }
     ExitStatus::of_code(status.code())
     //TODO @mverleg:
+}
+
+fn sound_notification(args: &MonArgs, is_success: bool) -> Result<(), String> {
+    let cursor = if is_success && args.sound_success {
+        Cursor::new(include_bytes!("/Users/mverleg/rusht/resource/success-sound.mp3").as_ref())
+    } else if !is_success && args.sound_failure {
+        Cursor::new(include_bytes!("/Users/mverleg/rusht/resource/error-sound.mp3").as_ref())
+        //TODO @mverleg: path
+    } else {
+        debug!("not playing sound because not requested for success={}", is_success);
+        return Ok(())
+    };
+    play_sound(cursor)
+}
+
+fn play_sound(sound_cursor: Cursor<&[u8]>) -> Result<(), String> {
+    let decoder = Decoder::new(sound_cursor)
+        .map_err(|err| format!("failed to decode sound, err: {}", err))?;
+    let (_, stream_handle) = OutputStream::try_default()
+        .map_err(|err| format!("failed to get default sound output device, err: {}", err))?;
+    stream_handle.play_raw(decoder.convert_samples())
+        .map_err(|err| format!("failed to play sound, err: {}", err))
 }
