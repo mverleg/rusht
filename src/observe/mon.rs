18fd028c1b26fca93ffe5f8205ff2d19044b8329
Mark Verleg <mark.verleg@imc.com>
2022-08-24 20:48:00 +0200

    Solve lifetime error, still fails to find device

--- src/observe/mon.rs
+++ src/observe/mon.rs
@@ -29,16 +29,12 @@ fn sound_notification(args: &MonArgs, is_success: bool) -> Result<(), String> {
         Cursor::new(include_bytes!("../../resource/success-sound.mp3").as_ref())
     } else if !is_success && args.sound_failure {
         Cursor::new(include_bytes!("../../resource/error-sound.mp3").as_ref())
-        //TODO @mverleg: path
     } else {
         debug!("not playing sound because not requested for success={}", is_success);
         return Ok(())
     };
-    play_sound(cursor)
-}
-
-fn play_sound(sound_cursor: Cursor<&[u8]>) -> Result<(), String> {
-    let decoder = Decoder::new(sound_cursor)
+    //TODO @mverleg: for some reason I cannot extract below into a function because it complains about lifetime of cursor
+    let decoder = Decoder::new(cursor)
         .map_err(|err| format!("failed to decode sound, err: {}", err))?;
     let (_, stream_handle) = OutputStream::try_default()
         .map_err(|err| format!("failed to get default sound output device, err: {}", err))?;
