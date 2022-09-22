7203dcb981a4ecd6eebee342a44638d404bd8e25
Mark Verleg <mark@localhost>
2022-09-22 23:28:21 +0200

    State to separate module

--- src/rscript/rsh_build.rs
+++ src/rscript/rsh_build.rs
@@ -20,12 +20,10 @@ use ::serde::Serialize;
 
 use crate::rscript::rsh_context::RshContext;
 use crate::rscript::rsh_program::RshProg;
+use crate::rscript::rsh_state::{
+    check_should_refresh, read_prog_state, CARGO_SRC, DUMMY_ARGS_SRC, DUMMY_RUN_SRC, MAIN_SRC,
+};
 use crate::rscript::RshArgs;
-
-const CARGO_SRC: &str = include_str!("./template/Cargo.toml");
-const MAIN_SRC: &str = include_str!("./template/src/main.rs");
-const DUMMY_ARGS_SRC: &str = include_str!("./template/src/args.rs");
-const DUMMY_RUN_SRC: &str = include_str!("./template/src/run.rs");
 
 pub fn compile_rsh(context: &RshContext, prog: RshProg, args: &RshArgs) -> Result<PathBuf, String> {
     let prev_state = read_prog_state(context, &prog)?;
@@ -45,79 +43,6 @@ struct ProgState {
     rsh_hash: String,
     template_hash: String,
     last_compile_ts_ms: u128,
-}
-
-fn check_should_refresh(prog: &RshProg, prev_state: &Option<ProgState>) -> bool {
-    //TODO @mverleg: make logging conditional?
-    let name = prog.name();
-    if let Some(prev_state) = prev_state {
-        if !prev_state.path.is_file() {
-            debug!(
-                "previous executable for {name} was not found at '{}'",
-                prev_state.path.to_string_lossy()
-            );
-            eprintln!("recompiling {name} because the previous executable has disappeared");
-            return true;
-        }
-        let prog_hash = calc_hash(vec![&prog.code]);
-        if prev_state.prog_hash != prog_hash {
-            eprintln!("recompiling {name} because the script changed");
-            return true;
-        }
-        let rsh_hash = get_rsh_exe_hash();
-        if prev_state.rsh_hash != rsh_hash {
-            eprintln!("recompiling {name} because rsh was updated");
-            return true;
-        }
-        let template_hash = calc_hash(vec![CARGO_SRC, MAIN_SRC]);
-        if prev_state.template_hash != template_hash {
-            eprintln!("recompiling {name} because rsh has a new template");
-            return true;
-        }
-    } else {
-        eprintln!("compiling {name} because no previous state was found");
-        return true;
-    }
-    debug!("using cached value of {name} because nothing changed");
-    false
-}
-
-fn read_prog_state(context: &RshContext, prog: &RshProg) -> Result<Option<ProgState>, String> {
-    let pth = context.state_path_for(prog.name());
-    if !pth.exists() {
-        debug!(
-            "no program state for {} at '{}'",
-            prog.name(),
-            pth.to_string_lossy()
-        );
-        return Ok(None);
-    } else {
-        debug!(
-            "reading program state for {} from '{}'",
-            prog.name(),
-            pth.to_string_lossy()
-        );
-    }
-    let reader = OpenOptions::new()
-        .read(true)
-        .open(&pth)
-        .map(BufReader::new)
-        .map_err(|err| {
-            format!(
-                "failed to read rsh state from '{}', err {}",
-                pth.to_string_lossy(),
-                err
-            )
-        })?;
-    serde_json::from_reader::<_, ProgState>(reader)
-        .map(|v| Some(v))
-        .map_err(|err| {
-            format!(
-                "failed to read rsh state from '{}', err {}",
-                pth.to_string_lossy(),
-                err
-            )
-        })
 }
 
 /// Creates and compiles a fixed project directory, to cache dependencies. Returns directory.
@@ -226,40 +151,4 @@ fn write_file(base_pth: &Path, file: impl Into<PathBuf>, content: &str) -> Resul
             err
         )
     })
-}
-
-fn calc_hash(content: Vec<&str>) -> String {
-    let mut hasher = Sha256::new();
-    for text in content {
-        hasher.update(text.as_bytes());
-    }
-    let hash_out = hasher.finalize();
-    encode_config(hash_out, URL_SAFE_NO_PAD)
-}
-
-/// Returns the modified time in ms as a string, to be used for checking whether rsh changed.
-///
-/// Note that this isn't perfect - quite some changes will have no effect, and some behaviour
-/// changes may result solely form libraries, which are not included. It's good enough though.
-fn get_rsh_exe_hash() -> String {
-    match env::current_exe()
-        .ok()
-        .and_then(|pth| pth.metadata().ok())
-        .and_then(|meta| meta.modified().ok())
-        .and_then(|modi| modi.duration_since(UNIX_EPOCH).ok())
-        .map(|dur| dur.as_millis())
-    {
-        Some(ts_ms) => {
-            debug!(
-                "rsh at '{}' was last changed {} ms ago",
-                env::current_exe().unwrap().to_string_lossy(),
-                ts_ms
-            );
-            ts_ms.to_string()
-        }
-        None => {
-            debug!("could not get the timestamp of rsh executable, not including in refresh hash");
-            "".to_owned()
-        }
-    }
 }
