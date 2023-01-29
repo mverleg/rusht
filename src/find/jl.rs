1cd18c07826cf4322311d5c56cf57f33ef19c1c0
Mark Verleg <mark@localhost>
2023-01-29 22:12:35 +0100

    More test and logging

--- src/find/jl.rs
+++ src/find/jl.rs
@@ -12,7 +12,6 @@ pub async fn list_files(
     writer: &mut impl LineWriter,
 ) -> ExitStatus {
     assert!(!args.no_recurse_symlinks, "no_recurse_symlinks not impl");
-    assert!(!args.entry_per_lines, "entry_per_lines not impl");
     if args.max_depth == 0 {
         eprintln!("jq max-depth is 0, likely should be at least 1")
     }
@@ -26,7 +25,8 @@ pub async fn list_files(
         line.push('[');
     }
     //TODO @mverleg: async walk dir?
-    for file_res in WalkDir::new(args.root).into_iter() {
+    let walker = WalkDir::new(args.root).max_depth(args.max_depth.try_into().expect("max depth too large"));
+    for file_res in walker.into_iter() {
         let file: DirEntry = match file_res {
             Ok(file) => file,
             Err(err) => {
@@ -58,9 +58,17 @@ pub async fn list_files(
             changed_by: "".to_string(),
             //TODO @mverleg: TEMPORARY! REMOVE THIS!
         };
+        if is_first {
+            is_first = false;
+        } else {
+            if ! args.entry_per_lines {
+                line.push(',');
+            }
+        }
         line.push_str(&serde_json::to_string(&node).expect("failed to create json from FSNode"));
         // unnecessary allocation but not performance-critical ^
         writer.write_line(&line).await;
+        eprintln!("line = {}", &line);  //TODO @mverleg: TEMPORARY! REMOVE THIS!
         line.clear();
     }
     if ! args.entry_per_lines {
@@ -73,8 +81,12 @@ pub async fn list_files(
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
+    use ::std::fs;
+
+    use ::regex::Regex;
+
     use crate::common::CollectorWriter;
+
     use super::*;
 
     #[async_std::test]
@@ -88,10 +100,10 @@ mod tests {
         let args = JlArgs {
             max_depth: 0,
             no_recurse_symlinks: false,
-            entry_per_lines: false,
+            entry_per_lines: true,
             filter: None,
-            on_error: Default::default(),
-            root: Default::default(),
+            on_error: ErrorHandling::Abort,
+            root: dir_path.to_owned(),
         };
 
         let mut writer = CollectorWriter::new();
@@ -101,5 +113,38 @@ mod tests {
 
         assert!(status.is_ok());
         assert_eq!(lines.len(), 3);
+        assert!(!lines[0].starts_with('['));
+        assert!(!lines[1].ends_with(','));
+        assert!(!lines[2].ends_with(']'));
+    }
+
+    #[async_std::test]
+    async fn deep_filtered_list_files_as_json_list() {
+        let dir_handle = tempfile::tempdir().unwrap();
+        let dir_path = dir_handle.path();
+        fs::write(dir_path.join("file1.txt"), "(no content)").unwrap();
+        fs::write(dir_path.join("file2"), "(no content)").unwrap();
+        fs::create_dir_all(dir_path.join("subdir")).unwrap();
+        //TODO @mverleg: create deeper directories and mismatching files
+
+        let args = JlArgs {
+            max_depth: 1000,
+            no_recurse_symlinks: false,
+            entry_per_lines: false,
+            filter: Some(Regex::new("^needle.*$").unwrap()),
+            on_error: ErrorHandling::FailAtEnd,
+            root: dir_path.to_owned(),
+        };
+
+        let mut writer = CollectorWriter::new();
+        let line_container = writer.lines();
+        let status = list_files(args, &mut writer).await;
+        let lines = line_container.snapshot().await;
+
+        assert!(status.is_ok());
+        assert_eq!(lines.len(), 3);
+        assert!(lines[0].starts_with('['));
+        assert!(lines[1].ends_with(','));
+        assert!(lines[2].ends_with(']'));
     }
 }
