908f17dc237aa981350ab81f1e982cf1aa6b1424
Mark Verleg <mark@localhost>
2023-01-29 21:26:49 +0100

    Create api type for file/dir

--- src/find/jl.rs
+++ src/find/jl.rs
@@ -1,3 +1,6 @@
+use ::log::debug;
+
+use ::walkdir::DirEntry;
 use ::walkdir::WalkDir;
 
 use crate::common::LineWriter;
@@ -13,17 +16,26 @@ pub async fn list_files(
 
     //TODO @mverleg: filter
     //TODO @mverleg: root
+    let mut has_err = false;
+    //TODO @mverleg: async walk dir?
     for file_res in WalkDir::new(args.root).into_iter() {
-        let file = match file_res {
+        let file: DirEntry = match file_res {
             Ok(file) => file,
-            Err(_) => match args.on_error {
-                ErrorHandling::Abort => todo!(),
-                ErrorHandling::FailAtEnd => todo!(),
-                ErrorHandling::Warn => todo!(),
-                ErrorHandling::Ignore => todo!(),
+            Err(err) => {
+                match args.on_error {
+                    ErrorHandling::Abort => {
+                        eprintln!("failed to read file, error: {err}");
+                        return ExitStatus::of(1)
+                    },
+                    ErrorHandling::FailAtEnd => { has_err = true; }
+                    ErrorHandling::Warn => eprintln!("failed to read file, error: {err}"),
+                    ErrorHandling::Ignore => debug!("ignoring file read error: {err}"),
+                }
+                continue
             }
         };
-        writer.write_line(file.path().display()).await
+        let name = file.path().display();
+        writer.write_line(name).await
     }
 
     todo!();
--- src/find/jl_json_api.rs
+++ src/find/jl_json_api.rs
@@ -1,1 +1,34 @@
+use ::std::cmp::Ordering;
 
+#[derive(Debug, Clone)]
+struct FSNode {
+    name: String,
+    base_name: String,
+    extension: String,
+    rel_path: String,
+    canonical_path: String,
+    is_dir: bool,
+    is_link: bool,
+    created: (),  //TODO @mverleg:
+    changed: (),  //TODO @mverleg:
+}
+
+impl PartialEq for FSNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.canonical_path.eq(&other.canonical_path)
+    }
+}
+
+impl Eq for FSNode {}
+
+impl PartialOrd for FSNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.canonical_path.partial_cmp(&other.canonical_path)
+    }
+}
+
+impl Ord for FSNode {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.canonical_path.cmp(&other.canonical_path)
+    }
+}
