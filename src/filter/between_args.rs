d88db9257e30de2bc128a6dec6a09b48f552022f
Mark Verleg <mark@localhost>
2023-03-02 20:34:57 +0100

    Define args for `between`

--- src/filter/between_args.rs
+++ src/filter/between_args.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use ::clap::Parser;
 
 use ::regex::Regex;
@@ -8,15 +9,41 @@ use ::regex::Regex;
     about = "Select all lines between two matches"
 )]
 pub struct BetweenArgs {
-    #[arg(short = 'f', long)]
+    #[arg(short = 'f', long, default_value = ".")]
     /// Start collecting lines when this expression matches
     pub from: Regex,
-    #[arg(short = 't', long)]
+    #[arg(short = 't', long, default_value = "\0")]
     /// Stop collecting lines when this expression matches
-    pub upto: Regex,
+    pub to: Regex,
+    #[arg(short = 'F', long, default_value = "include")]
+    /// How to handle the matched --from line, include [i] of exclude [e]
+    pub from_handling: MatchHandling,
+    #[arg(short = 'T', long, default_value = "exclude")]
+    /// How to handle the matched --to line, include [i] of exclude [e]
+    pub to_handling: MatchHandling,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MatchHandling {
+    Inclusive,
+    Exclusive,
+}
+
+impl FromStr for MatchHandling {
+    type Err = String;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        Ok(match text.to_lowercase().as_str() {
+            "include" | "i" => MatchHandling::Inclusive,
+            "skip" | "s" | "exclude" | "e" => MatchHandling::Exclusive,
+            other => return Err(format!("unknown match handling mode: {}", other)),
+        })
+    }
 }
 
 #[test]
 fn test_cli_args() {
-    BetweenArgs::try_parse_from(&["cmd", "--", "-f", ".*", "-t", "^END$"]).unwrap();
+    BetweenArgs::try_parse_from(&["cmd", "--from", ".*"]).unwrap();
+    BetweenArgs::try_parse_from(&["cmd", "--to", "^END$", "-T", "i"]).unwrap();
+    BetweenArgs::try_parse_from(&["cmd", "-f", ".*", "-F", "i", "-t", "^END$", "-T", "s"]).unwrap();
 }
