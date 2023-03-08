37166db2737f5de636dca4d71e8ab4e96b82c893
Mark Verleg <mark@localhost>
2023-03-08 23:31:39 +0100

    Allow more symbols that do not need to be quoted

--- src/common/task.rs
+++ src/common/task.rs
@@ -13,7 +13,7 @@ use dirs::home_dir;
 use crate::common::resolve_executable;
 
 lazy_static! {
-    static ref SAFE_ARG_RE: Regex = Regex::new(r"^[\p{L}0-9_\-\.,@/:]+$").unwrap();
+    static ref SAFE_ARG_RE: Regex = Regex::new(r"^[\p{L}0-9_\-\.,@/:=\+]+$").unwrap();
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
