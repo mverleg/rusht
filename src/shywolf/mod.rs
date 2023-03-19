d11489b3f8c3fc39e8a252c4e9f36a60d9fe4150
Mark Verleg <mark@localhost>
2023-03-19 23:25:48 +0100

    Impl struct type checks

--- src/shywolf/mod.rs
+++ src/shywolf/mod.rs
@@ -1,5 +1,4 @@
 use ::std::collections::HashMap;
-use ::std::fmt;
 use ::std::sync::RwLock;
 
 use ::lazy_static::lazy_static;
@@ -10,13 +9,6 @@ lazy_static! {
 
 pub struct TypeRegistry {
     content: RwLock<TypeRegistryContent>,
-}
-
-impl fmt::Debug for TypeRegistry {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut content = self.content.write().expect("lock poisoned");
-        write!(f, "{:?}", &*content)
-    }
 }
 
 #[derive(Debug)]
@@ -52,7 +44,9 @@ impl TypeRegistry {
             name: name.to_string(),
             kind: TypeKind::Struct {},
         });
-        Type { id: rank }
+        let typ = Type { id: rank };
+        content.lookup.insert(name.to_owned(), typ);
+        typ
     }
 
     pub fn lookup(&self, name: &str) -> Option<Type> {
@@ -88,7 +82,7 @@ impl Type {
     /// x: ThisType = ArgumentType::new()
     pub fn is_assignable_from(&self, value: Type) -> bool {
         let types = &TYPES.content.read().expect("lock poisoned").all;
-        //TODO @mverleg: not ideal to access `content` directly, but whatever
+        //TODO @mverleg: not ideal to access `content` directly, but whatevera
         let left = &types[self.id];
         let right = &types[value.id];
         match (&left.kind, &right.kind) {
@@ -104,7 +98,7 @@ mod tests {
 
     #[test]
     fn test_concrete_identical() {
-        dbg!(&TYPES);
+        dbg!(&TYPES.content.read());
         let nr = TYPES.lookup("int").unwrap();
         assert!(nr.is_assignable_from(nr));
     }
