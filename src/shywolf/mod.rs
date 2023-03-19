dea78cc836215f604fe5d079c0ff986feafd6c87
Mark Verleg <mark@localhost>
2023-03-19 22:25:03 +0100

    Init a few default tyoes

--- src/shywolf/mod.rs
+++ src/shywolf/mod.rs
@@ -1,10 +1,11 @@
 use ::std::collections::HashSet;
 use ::std::sync::RwLock;
+use std::collections::HashMap;
 
 use ::lazy_static::lazy_static;
 
 lazy_static! {
-    static ref TYPES: TypeRegistry = TypeRegistry::new();
+    static ref TYPES: TypeRegistry = TypeRegistry::init();
 }
 
 #[derive(Debug)]
@@ -15,7 +16,7 @@ pub struct TypeRegistry {
 #[derive(Debug)]
 pub struct TypeRegistryContent {
     all: Vec<TypeInfo>,
-    seen: HashSet<String>,
+    lookup: HashMap<String, Type>,
 }
 
 impl TypeRegistry {
@@ -23,15 +24,24 @@ impl TypeRegistry {
         TypeRegistry {
             content: RwLock::new(TypeRegistryContent {
                 all: Vec::new(),
-                seen: HashSet::new(),
+                lookup: HashMap::new(),
             })
         }
     }
 
+    pub fn init() -> Self {
+        let types = Self::new();
+        types.add_struct("int");
+        types.add_struct("string");
+        types
+    }
+
     pub fn add_struct(&self, name: &str) -> Type {
         let mut content = self.content.write().expect("lock poisoned");
-        assert!(content.seen.insert(name.to_owned()));
         let rank = content.all.len();
+        if content.lookup.contains_key(name) {
+            panic!("type already defined: {name}'")
+        }
         content.all.push(TypeInfo {
             name: name.to_string(),
             kind: TypeKind::Struct {},
@@ -64,7 +74,7 @@ pub struct Type {
 impl Type {
     /// is this valid?
     /// x: ThisType = ArgumentType::new()
-    pub fn is_assignable_from(&self, value: Type) {
+    pub fn is_assignable_from(&self, value: Type) -> bool {
         todo!()
     }
 }
@@ -75,14 +85,14 @@ mod tests {
 
     #[test]
     fn test_concrete_identical() {
-        let nr = TYPES.add_struct("int");
+        let nr = TYPES.lookup("int");
         assert!(nr.is_assignable_from(nr));
     }
 
     #[test]
     fn test_concrete_mismatch_structs() {
-        let nr = TYPES.add_struct("int");
-        let txt = TYPES.add_struct("string");
+        let nr = TYPES.lookup("int");
+        let text = TYPES.lookup("string");
         assert!(!nr.is_assignable_from(txt));
     }
 }
