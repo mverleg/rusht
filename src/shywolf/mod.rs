906cc84b198d7cf49478d263386e713c5cbaad74
Mark Verleg <mark@localhost>
2023-03-26 20:40:48 +0200

    Create dummy locator and some test types

--- src/shywolf/mod.rs
+++ src/shywolf/mod.rs
@@ -3,11 +3,21 @@
 
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::sync::atomic::{AtomicU32, Ordering};
+
+static DUMMY_LOC_COUNTER: AtomicU32 = AtomicU32::new(0);
 
 /// Source file location; dummy for now
 #[derive(Debug)]
 struct Loc {
     pub pos: u32,
+}
+
+#[cfg(test)]
+impl Loc {
+    pub fn dummy() -> Loc {
+        Loc { pos: DUMMY_LOC_COUNTER.fetch_add(1, Ordering::AcqRel) }
+    }
 }
 
 #[derive(Debug)]
@@ -30,7 +40,7 @@ enum TypeKind {
 #[derive(Debug)]
 struct AST {
     structs: Vec<(String, Loc)>,
-    interfaces: Vec<(String, Loc)>,
+    interfaces: Vec<(String, Loc, bool)>,
     implementations: Vec<(Type, Type, Loc)>,
 }
 
@@ -45,6 +55,10 @@ impl AST {
 
     pub fn declare_struct(&mut self, name: impl Into<String>, loc: Loc) {
         self.structs.push((name.into(), loc));
+    }
+
+    pub fn declare_interface(&mut self, name: impl Into<String>, loc: Loc, is_sealed: bool) {
+        self.interfaces.push((name.into(), loc, is_sealed));
     }
 }
 
@@ -62,8 +76,12 @@ mod tests {
 
     fn build_test_ast() -> AST {
         let mut ast = AST::new();
-        ast.declare_struct("int", loc);
-
+        ast.declare_struct("int", Loc::dummy());
+        ast.declare_struct("float", Loc::dummy());
+        ast.declare_struct("String", Loc::dummy());
+        ast.declare_struct("Password", Loc::dummy());
+        ast.declare_interface("Display", Loc::dummy(), false);
+        ast.declare_interface("Add", Loc::dummy(), false);
         ast
     }
 
