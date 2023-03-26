8366fe0ba56b5900c9f44ba015238472dfe43045
Mark Verleg <mark@localhost>
2023-03-26 20:37:30 +0200

    Basic AST

--- src/shywolf/mod.rs
+++ src/shywolf/mod.rs
@@ -6,25 +6,70 @@ use std::collections::HashSet;
 
 /// Source file location; dummy for now
 #[derive(Debug)]
-pub struct Loc {
+struct Loc {
     pub pos: u32,
 }
 
 #[derive(Debug)]
-pub enum TypeErr {
+enum TypeErr {
     //
 }
 
 #[derive(Debug)]
-pub struct Type {
-    //
+struct Type {
+    pub id: usize,
+    name: String,
+}
+
+#[derive(Debug)]
+enum TypeKind {
+    Struct,
+    Interface { sealed: bool, },
 }
 
 #[derive(Debug)]
 struct AST {
-    impls: Vec<(Type, Type, Loc)>,
+    structs: Vec<(String, Loc)>,
+    interfaces: Vec<(String, Loc)>,
+    implementations: Vec<(Type, Type, Loc)>,
+}
+
+impl AST {
+    pub fn new() -> Self {
+        AST {
+            structs: Vec::new(),
+            interfaces: Vec::new(),
+            implementations: Vec::new(),
+        }
+    }
+
+    pub fn declare_struct(&mut self, name: impl Into<String>, loc: Loc) {
+        self.structs.push((name.into(), loc));
+    }
 }
 
 #[derive(Debug)]
 struct TypeContext {}
 
+fn check_types(ast: &AST) -> Result<TypeContext, Vec<TypeErr>> {
+    todo!()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn build_test_ast() -> AST {
+        let mut ast = AST::new();
+        ast.declare_struct("int", loc);
+
+        ast
+    }
+
+    #[test]
+    fn test_add() {
+        let ast = build_test_ast();
+        check_types(&ast).unwrap();
+    }
+}
