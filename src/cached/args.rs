c0ffee6360a1cbc887c7953702861006c9a3345c
Mark Verleg <mark@localhost>
2024-02-20 22:29:37 +0100

    Add long names to various cached args

--- src/cached/args.rs
+++ src/cached/args.rs
@@ -17,32 +17,32 @@ pub struct CachedArgs {
     #[arg(value_parser = parse_dur, short = 'd', long = "duration", default_value = "15 min")]
     pub duration: Duration,
     /// Invalidates cache if the current git HEAD commit is different.
-    #[arg(short = 'g')]
+    #[arg(short = 'g', long)]
     pub git_head: bool,
     /// Invalidates cache if the current git branch merge-base commit is different.
-    #[arg(short = 'b')]
+    #[arg(short = 'b', long)]
     pub git_base: bool,
     /// Invalidates cache if the uncommitted git files change.
-    #[arg(short = 'p')]
+    #[arg(short = 'p', long)]
     pub git_pending: bool,
-    #[arg(short = 'e')]
+    #[arg(short = 'e', long)]
     pub env: Vec<String>,
     /// Just a text. Invalidates cache if a different text is passed.
-    #[arg(short = 't')]
+    #[arg(short = 't', long)]
     pub text: Vec<String>,
     /// Does NOT invalidate cache if the command is run from a different directory.
-    #[arg(short = 'D')]
+    #[arg(short = 'D', long)]
     pub no_dir: bool,
     /// Does NOT cache if a different command is run. Should perhaps be used with e.g. --text
-    #[arg(short = 'C')]
+    #[arg(short = 'C', long)]
     pub no_command: bool,
     /// Does NOT cache if different env vars are passed to the command (does not include inherited env)
-    #[arg(short = 'E')]
+    #[arg(short = 'E', long)]
     pub no_direct_env: bool,
     #[arg(short = 's', long)]
     pub no_cached_output: bool,
     /// Use exit code 0 if the command is cached, and exit code 255 if it ran successfully.
-    #[arg(short = 'e', long)]
+    #[arg(short = 'x', long)]
     pub exit_code: bool,
     /// Print extra information, e.g. whether the command was run or not.
     #[arg(short = 'v', long)]
@@ -82,12 +82,12 @@ fn test_cli_args() {
     let mut args = CachedArgs::try_parse_from(&["cmd", "ls"]).unwrap();
     assert!(!args.any_explicit_key());
     assert_eq!(args, CachedArgs {
-        cmd: CommandArgs::Cmd(vec!["cmd".to_owned(), "ls".to_owned()]),
+        cmd: CommandArgs::Cmd(vec!["ls".to_owned()]),
         ..CachedArgs::default()
     });
     args = CachedArgs::try_parse_from(&["cmd", "--duration", "1 year", "ls"]).unwrap();
     assert!(!args.any_explicit_key());
-    args = CachedArgs::try_parse_from(&["cmd", "-d1y", "--git-head", "--git_pending", "ls", "-a", "-l", "-s", "-h"]).unwrap();
+    args = CachedArgs::try_parse_from(&["cmd", "-d1y", "--git-head", "--git-pending", "ls", "-a", "-l", "-s", "-h"]).unwrap();
     assert!(args.any_explicit_key());
     args = CachedArgs::try_parse_from(&["cmd", "-d1y", "--text", "string", "ls", "-alsh"]).unwrap();
     assert!(args.any_explicit_key());
