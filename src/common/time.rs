c0ffeea8daf1049b2847158c443fae68121f1bf8
Mark Verleg <mark@localhost>
2024-06-03 21:17:10 +0200

    Note about timezones

--- src/common/time.rs
+++ src/common/time.rs
@@ -2,7 +2,8 @@ use ::time::OffsetDateTime;
 use ::time::macros::format_description;
 
 pub fn current_time_user_str() -> String {
-    // "[year]-[month]-[day] [hour]:[minute]:[second] [offset_hour sign:mandatory]:[offset_minute]:[offset_second]"
+    // the `time` crate always fails on Mac and Linux when inferring timezone
+    // because  of a vulnerability in libc when getting timezones
     let format = format_description!("[hour]:[minute]:[second]");
     match OffsetDateTime::now_local() {
         Ok(time) => time.format(format).unwrap(),
